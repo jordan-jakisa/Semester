820ac042c4473bad7f68d3879cd384452020aa14
Ssemujju Sharif <shinshray@gmail.com>
2022-07-05 17:21:04 +0300

    First Commit

---
+++ app/src/main/java/com/bawano/semester/models/Course.kt
@@ -0,0 +1,15 @@
+package com.bawano.semester.models
+
+import android.os.Parcelable
+import androidx.recyclerview.widget.DiffUtil
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Course(var title: String?, var nickname: String?, var code: String?, var image:String?, var description: String?) : Parcelable
+
+object CourseCallback : DiffUtil.ItemCallback<Course>() {
+    override fun areItemsTheSame(oldItem: Course, newItem: Course) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: Course, newItem: Course) = oldItem.code == newItem.code
+}
+
+
