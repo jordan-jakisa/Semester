e66741966af4cf3d5c2212b26324972cffe3cf39
Sharif Ssemujju <sharifcreed@gmail.com>
2023-03-08 22:25:37 +0300

    Designed the home screen

--- app/src/main/java/com/bawano/semester/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bawano/semester/ui/home/HomeFragment.kt
@@ -1,24 +1,15 @@
 package com.bawano.semester.ui.home
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import com.bawano.semester.databinding.FragmentHomeBinding
-import com.bawano.semester.utils.Constants.COURSES
-import com.bawano.semester.utils.Constants.DETAILS
-import com.bawano.semester.utils.Constants.PDFVIEW
-import com.bawano.semester.utils.PreferenceManager
-import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
     private lateinit var b: FragmentHomeBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
